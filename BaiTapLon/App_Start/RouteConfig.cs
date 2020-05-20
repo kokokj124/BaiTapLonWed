e1f0b7d5b7b04be29fa9909ad8b43ee90e6d1786
kokokj124 <49259557+kokokj124@users.noreply.github.com>
2020-05-20 22:18:40 +0700

    Add project files.

---
+++ BaiTapLon/App_Start/RouteConfig.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace BaiTapLon
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+            name: "Index",
+            url: "index/{id}",
+            defaults: new { controller = "BaiTapLon", action = "Index", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+            name: "Details",
+            url: "details/{id}",
+            defaults: new { controller = "BaiTapLon", action = "Details", id = UrlParameter.Optional }
+            );
+
+
+            routes.MapRoute(
+            name: "Checkout",
+            url: "checkout/{id}",
+            defaults: new { controller = "BaiTapLon", action = "Checkout", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+            name: "Contact",
+            url: "contact/{id}",
+            defaults: new { controller = "BaiTapLon", action = "Contact", id = UrlParameter.Optional }
+            );
+
+
+            routes.MapRoute(
+            name: "Login",
+            url: "login/{id}",
+            defaults: new { controller = "BaiTapLon", action = "Login", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+            name: "Register",
+            url: "register/{id}",
+            defaults: new { controller = "BaiTapLon", action = "Register", id = UrlParameter.Optional }
+            );
+
+
+            routes.MapRoute(
+            name: "Women",
+            url: "women/{id}",
+            defaults: new { controller = "BaiTapLon", action = "Women", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "BaiTapLon", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ BaiTapLon/Controllers/BaiTapLonController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BaiTapLon.Controllers
+{
+    public class BaiTapLonController : Controller
+    {
+        // GET: BaiTapLon
+        public ActionResult Index()
+        {
+            return View("~/Views/BaiTapLon/index.cshtml");
+        }
+        public ActionResult Women()
+        {
+            return View("~/Views/BaiTapLon/women.cshtml");
+        }
+        public ActionResult Checkout()
+        {
+            return View("~/Views/BaiTapLon/checkout.cshtml");
+        }
+        public ActionResult Contact()
+        {
+            return View("~/Views/BaiTapLon/contact.cshtml");
+        }
+        public ActionResult Details()
+        {
+            return View("~/Views/BaiTapLon/details.cshtml");
+        }
+        public ActionResult Login()
+        {
+            return View("~/Views/BaiTapLon/login.cshtml");
+        }
+        public ActionResult Register()
+        {
+            return View("~/Views/BaiTapLon/register.cshtml");
+        }
+    }
+}
